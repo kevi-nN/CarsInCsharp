25f24435c62682ca9338b57a282476ad05efc094
kevi-nN <kqn2@pitt.edu>
2023-01-23 20:27:33 -0500

    First Version with menu
    Add cars but doesn't add or delete cars

---
+++ CarsProject/CarsProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CarsProject // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the year of the vehicle:");
+            Car car = new Car();
+            int year = int.Parse(Console.ReadLine());
+            car.year = year;
+            Console.WriteLine("Enter the make of the vehicle:");
+            string make = Console.ReadLine();
+            car.make = make;
+            Console.WriteLine("Enter the model of the vehicle:");
+            string model =Console.ReadLine();
+            car.model = model;
+            Console.WriteLine("Enter the number of doors on the vehicle:");
+            int numOfDoors = int.Parse(Console.ReadLine());
+            car.numberOfDoors = numOfDoors;
+            Console.WriteLine("Enter the price of the vehicle:");
+            double price = double.Parse(Console.ReadLine());
+            car.price = price;
+
+            Console.WriteLine("Your car is:" + car.year.ToString() + " " + car.make + " " + car.model);
+
+        }
+    }
+}
