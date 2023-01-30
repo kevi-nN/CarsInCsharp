262a92f0b3c97aa2ae6533a8397156af298b5846
kevi-nN <kqn2@pitt.edu>
2023-01-30 18:53:07 -0500

    Update Program.cs
    final code

--- CarsProject/CarsProject/Program.cs
+++ CarsProject/CarsProject/Program.cs
@@ -4,75 +4,82 @@ namespace CarsProject // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        List<Car> cars = new List<Car>();
+        static List<Car> cars = new List<Car>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the year of the vehicle:");
-            Car car = new Car();
-            int year = int.Parse(Console.ReadLine());
-            car.year = year;
-            Console.WriteLine("Enter the make of the vehicle:");
-            string make = Console.ReadLine();
-            car.make = make;
-            Console.WriteLine("Enter the model of the vehicle:");
-            string model = Console.ReadLine();
-            car.model = model;
-            Console.WriteLine("Enter the number of doors on the vehicle:");
-            int numOfDoors = int.Parse(Console.ReadLine());
-            car.numberOfDoors = numOfDoors;
-            Console.WriteLine("Enter the price of the vehicle:");
-            double price = double.Parse(Console.ReadLine());
-            car.price = price;
 
-            Console.WriteLine("Your car is:" + " " + car.year.ToString() + " " + car.make + " " + car.model);
 
-        }
-        // main menu
-        // prompt for user input, show menu until 4
+            // main menu
+            // prompt for user input, show menu until 4
 
-        string input = "";
+            string input = "";
 
-        while (input != "4")
-        {
-            Console.WriteLine(" Welcome to kevins dealership");
-            Console.WriteLine(" 1. Add a new car");
-            Console.WriteLine(" 2. Sell a current car");
-            Console.WriteLine(" 3. List all cars");
-            Console.WriteLine(" 4 to quit");
-            Console.ReadLine();
-        }
-
-        public void AddCar()
-        {
-            Console.WriteLine("Enter the year of the vehicle:");
-            Car car = new Car();
-            int year = int.Parse(Console.ReadLine());
-            car.year = year;
-            Console.WriteLine("Enter the make of the vehicle:");
-            string make = Console.ReadLine();
-            car.make = make;
-            Console.WriteLine("Enter the model of the vehicle:");
-            string model = Console.ReadLine();
-            car.model = model;
-            Console.WriteLine("Enter the number of doors on the vehicle:");
-            int numOfDoors = int.Parse(Console.ReadLine());
-            car.numberOfDoors = numOfDoors;
-            Console.WriteLine("Enter the price of the vehicle:");
-            double price = double.Parse(Console.ReadLine());
-            car.price = price;
-            cars.Add(car);
-            Console.WriteLine("Your car is:" + " " + car.year.ToString() + " " + car.make + " " + car.model);
-        }
-        public void DeleteCar()
-        {
-
-        }
-        public void ListCars()
-        {
-            foreach (Car car in cars)
+            while (input != "4")
             {
-                Console.WriteLine(car.year.ToString() + " " + car.make + " " + car.model + " " + car.price.ToString());
+                Console.WriteLine(" Welcome to kevins dealership");
+                Console.WriteLine(" 1. Add a new car");
+                Console.WriteLine(" 2. Sell a current car");
+                Console.WriteLine(" 3. List all cars");
+                Console.WriteLine(" 4 to quit");
+                Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        AddCar();
+                        break;
+                    case "2":
+                        DeleteCar();
+                        break;
+                    case "3":
+                        ListCars();
+                        break;
+                    default:
+                        break;
+
+                }
             }
         }
-    }
-}
+                public static void AddCar()
+                {
+                    Console.WriteLine("Enter the year of the vehicle:");
+                    Car car = new Car();
+                    int year = int.Parse(Console.ReadLine());
+                    car.year = year;
+                    Console.WriteLine("Enter the make of the vehicle:");
+                    string make = Console.ReadLine();
+                    car.make = make;
+                    Console.WriteLine("Enter the model of the vehicle:");
+                    string model = Console.ReadLine();
+                    car.model = model;
+                    Console.WriteLine("Enter the number of doors on the vehicle:");
+                    int numOfDoors = int.Parse(Console.ReadLine());
+                    car.numberOfDoors = numOfDoors;
+                    Console.WriteLine("Enter the price of the vehicle:");
+                    double price = double.Parse(Console.ReadLine());
+                    car.price = price;
+                    cars.Add(car);
+                    Console.WriteLine("Your car is:" + " " + car.year.ToString() + " " + car.make + " " + car.model);
+                }
+                public static void DeleteCar()
+                {
+                    ListCars();
+                    //ask the user which car to delete
+                    Console.WriteLine("Enter the number of the vehicle you want ot remove: ");
+                    //delete that car from the list
+                    int carToRemove = int.Parse(Console.ReadLine());    
+                    cars.RemoveAt(carToRemove);
+                    Console.WriteLine("Car Removed");
+           
+                }
+                public static void ListCars()
+                {
+                    int i = 1;
+                    foreach (Car car in cars)
+                    {
+                        Console.WriteLine("Car #" + i + " " + car.year.ToString() + " " + car.make + " " + car.model + " " + car.price.ToString());
+                        i++;
+                    }
+                }
+            }
+        }
+    
